9142ec2fa07aa4b0d40a56024b510810a25fd3b3
CR <1061667048@qq.com>
2017-05-08 17:06:45 +0800

    增加junit

--- src/Homework/Homework.java
+++ src/Homework/Homework.java
@@ -1,5 +1,9 @@
 package Homework;
 
+import java.util.Scanner;
+
+import org.junit.Test;
+
 public class Homework {
 	public static void main(String[] args){
 		int a = 3, b = 4;
@@ -8,5 +12,16 @@ public class Homework {
 		a = b;
 		b = temp;
 		System.out.println("after: a=" + a + " b=" + b);
+	}
+	
+    @Test
+	public void Test(){
+		Scanner scanner = new Scanner(System.in);
+		int year = scanner.nextInt();
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+			System.out.println("是闰年");
+		} else {
+			System.out.println("不是闰年");
+		}
 	}
 }
